0fd62db91c7f53e54ed7a21ae9ed3977db30048d
tang <2649212868@qq.com>
2017-12-06 15:32:21 +0800

    测试网络框架和loader加载图标

--- app/src/main/java/hipad/com/fastdian/example/ExampleDelegate.java
+++ app/src/main/java/hipad/com/fastdian/example/ExampleDelegate.java
@@ -35,10 +35,11 @@ public class ExampleDelegate extends LatteDelegate{
     private void testRestClient(){
         RestClient.builder()
                 .url("http://news.baidu.com/")
+                .loader(getContext())
                 .success(new ISuccess() {
                     @Override
                     public void onSuccess(String response) {
-                        Toast.makeText(getContext(),response,Toast.LENGTH_LONG).show();
+                       // Toast.makeText(getContext(),response,Toast.LENGTH_LONG).show();
 
                     }
                 })
